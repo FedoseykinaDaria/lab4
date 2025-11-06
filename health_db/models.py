db571e6708970636774ebd236c68d3afefc76be3
Fedoseykina_Daria <fedoseykina-daria@mail.ru>
2025-11-06 09:36:59 +0500

    Add note model

--- health_db/models.py
+++ health_db/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Note(models.Model):
+    name = models.CharField(max_length = 200)
+    age = models.IntegerField(min_value = 1)
+    pressureUP = models.IntegerField(min_value = 0)
+    pressureDOWN = models.IntegerField(min_value = 0)
+    cholesterol = models.IntegerField(min_value = 0)
+    glucose = models.IntegerField(min_value = 0)
+    sleep_time = models.IntegerField(min_value = 0)
+    BMI = models.IntegerField(min_value = 0)
