a64d6bd700b704bc029188111ec834a1f32c764b
Fedoseykina_Daria <fedoseykina-daria@mail.ru>
2025-11-06 09:50:05 +0500

    Add migrations

--- health_db/models.py
+++ health_db/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 class Note(models.Model):
     name = models.CharField(max_length = 200)
-    age = models.IntegerField(min_value = 1)
-    pressureUP = models.IntegerField(min_value = 0)
-    pressureDOWN = models.IntegerField(min_value = 0)
-    cholesterol = models.IntegerField(min_value = 0)
-    glucose = models.IntegerField(min_value = 0)
-    sleep_time = models.IntegerField(min_value = 0)
-    BMI = models.IntegerField(min_value = 0)
+    age = models.IntegerField()
+    pressureUP = models.IntegerField()
+    pressureDOWN = models.IntegerField()
+    cholesterol = models.IntegerField()
+    glucose = models.IntegerField()
+    sleep_time = models.IntegerField()
+    BMI = models.IntegerField()
