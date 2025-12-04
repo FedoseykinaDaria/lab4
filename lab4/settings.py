6bf7ae5b9d857d309e2ec22599f11b60abfaf2c4
Fedoseykina_Daria <fedoseykina-daria@mail.ru>
2025-12-04 21:22:52 +0500

    Add environment file

--- lab4/settings.py
+++ lab4/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0qtx4rbg2m0)^*shq&w-z)pofz&x&!t)jy*+7d*9fhyif8h^g@'
+SECRET_KEY = os.environ.get("SECRET_KEY")
+#'django-insecure-0qtx4rbg2m0)^*shq&w-z)pofz&x&!t)jy*+7d*9fhyif8h^g@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get("DEBUG", default=0))
+#True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+#[]
 
 
 # Application definition
